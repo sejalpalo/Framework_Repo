25d8c2057588c374c3b9e3d117851f49a8684ba4
sejalpalo <sejalpalo456@gmail.com>
2024-08-16 17:08:06 +0530

    added new test

--- SeleniumProject/src/test/java/Pac1/ContactTest.java
+++ SeleniumProject/src/test/java/Pac1/ContactTest.java
@@ -19,5 +19,9 @@ public class ContactTest {
   public void modifyContactTest() {
 	  System.out.println("execute modifycontactTest");
   }
+  @Test
+  public void deleteContactTest() {
+	  System.out.println("execute deletecontactTest");
+  }
   
 }
