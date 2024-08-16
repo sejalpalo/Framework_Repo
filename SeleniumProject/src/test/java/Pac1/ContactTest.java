f7c359133be80751ff16eb1adc0cbe8534f61b93
sejalpalo <sejalpalo456@gmail.com>
2024-08-16 16:28:49 +0530

    1st commit

---
+++ SeleniumProject/src/test/java/Pac1/ContactTest.java
@@ -0,0 +1,23 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+  @Test
+  public void CreatecontactTest() {
+	  String URL = System.getProperty("url");
+	  String BROWSER = System.getProperty("browser");
+	  String USERNAME = System.getProperty("username");
+	  String PASSWORD= System.getProperty("password");
+	  System.out.println(URL);
+	  System.out.println(BROWSER);
+	  System.out.println(USERNAME);
+	  System.out.println(PASSWORD);
+	  System.out.println("excute createcontactTest");
+  }
+  @Test
+  public void modifyContactTest() {
+	  System.out.println("execute modifycontactTest");
+  }
+  
+}
---
+++ SeleniumProject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,14 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+@Test
+public void createOrgTest() {
+	System.out.println("execute createorgTest");
+}
+@Test
+public void modifyOrgTest() {
+	System.out.println("execute modifyorgTest");
+}
+}
